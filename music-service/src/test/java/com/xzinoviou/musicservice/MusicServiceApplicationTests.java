924433aba270750712d7350c8d21dd411601b82b
xzinoviou <xzinoviou@gmail.com>
2019-07-13 01:39:41 +0300

    created music-service

---
+++ music-service/src/test/java/com/xzinoviou/musicservice/MusicServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xzinoviou.musicservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MusicServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
