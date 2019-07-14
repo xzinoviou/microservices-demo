45db161a62c0f2654c381046deffe79287843176
xzinoviou <xzinoviou@gmail.com>
2019-07-14 12:23:58 +0300

    edited zuul routes

--- zuul-service/src/main/java/com/xzinoviou/zuulservice/ZuulServiceApplication.java
+++ zuul-service/src/main/java/com/xzinoviou/zuulservice/ZuulServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulServiceApplication {
